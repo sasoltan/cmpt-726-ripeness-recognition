58a140afb7a2dcf3d30363fabca6b6c50956b5d6
Kyle Demeule <kyle.demeule@gmail.com>
2015-12-01 19:47:41 -0800

    Switching back to outputting 256x256 images. Incorporating mean alexnet image, put diff of image and mean into neural net.

--- extract_features.py
+++ extract_features.py
@@ -3,13 +3,23 @@ import numpy, sys, os, caffe, re, time
 caffe_root = '../caffe/'
 caffe_model = "bvlc_alexnet"
 output_layer = "fc7"
+network_dimensions = (227, 227, 3)
 
-def model_precheck(caffe_root, caffe_model):
-    if os.path.isfile(get_model_path(caffe_root, caffe_model)):
-        return True
-    else:
+def preflight_check(caffe_root, caffe_model):
+    model_path = get_model_path(caffe_root, caffe_model)
+    proto_path = get_proto_path(caffe_root, caffe_model)
+    mean_image_path = get_mean_image_path(caffe_root, caffe_model)
+    if not os.path.isfile(model_path):
         print("Pre-trained model not downloaded, please download the %s model." % (caffe_model))
         return False
+    elif not os.path.isfile(proto_path):
+        print("Can't find proto at %s." % (proto_path))
+        return False
+    elif not os.path.isfile(mean_image_path):
+        print("Can't find mean image at %s." % (mean_image_path))
+        return False
+    else:
+        return True
 
 def get_model_path(caffe_root, caffe_model):
     return "%smodels/%s/%s.caffemodel" % (caffe_root, caffe_model, caffe_model)
@@ -17,13 +27,32 @@ def get_model_path(caffe_root, caffe_model):
 def get_proto_path(caffe_root, caffe_model):
     return "%smodels/%s/deploy.prototxt" % (caffe_root, caffe_model)
 
+def get_mean_image_path(caffe_root, caffe_model):
+    return "%smodels/%s/imagenet_mean.binaryproto" % (caffe_root, caffe_model)
+
+def get_mean_image(caffe_root, caffe_model):
+    mean_image_path = get_mean_image_path(caffe_root, caffe_model)
+    blob = caffe.proto.caffe_pb2.BlobProto()
+    data = open(mean_image_path, 'rb' ).read()
+    blob.ParseFromString(data)
+    arr = numpy.array(caffe.io.blobproto_to_array(blob))
+    return arr[0].transpose((1, 2, 0))
+
 def batch(iterable, n=1):
     l = len(iterable)
     for ndx in range(0, l, n):
         yield iterable[ndx:min(ndx + n, l)]
 
+def process_image(image_url, mean_image):
+    image = caffe.io.load_image(image_url)
+    # subtract the mean
+    image = image - mean_image
+    # resize image
+    image = caffe.io.resize_image(image, network_dimensions)
+    return image.transpose((2, 0, 1))
+
 def main():
-    if not model_precheck(caffe_root, caffe_model):
+    if not preflight_check(caffe_root, caffe_model):
         return False
 
     pic_folder = sys.argv[1]
@@ -42,13 +71,14 @@ def main():
     layer_outputs = []
 
     caffe.set_mode_cpu()
+    mean_image = get_mean_image(caffe_root, caffe_model)
     proto_path = get_proto_path(caffe_root, caffe_model)
     model_path = get_model_path(caffe_root, caffe_model)
     net = caffe.Net(proto_path, model_path, caffe.TEST)
 
     start = time.time()
     for images in batch(image_list, 50):
-        np_images = numpy.array(map(lambda i: caffe.io.load_image(i).transpose((2, 0, 1)), images))
+        np_images = numpy.array(map(lambda i: process_image(i, mean_image), images))
 
         net.blobs['data'].reshape(len(images),3,227,227)
         net.blobs['data'].data[...] = np_images
